8d6d533b6fc2d60832ead02d245842734659d5df
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-13 00:41:55 -0400

    Completes lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    squared_list = []
+    for num in int_list:
+        squared_list.append(num ** 2)
+    return squared_list
     pass
 
 def fizzbuzz():
-    # code goes here!
+    num = 1
+    while num < 101:
+        if (num % 3 == 0 and num % 5 == 0):
+          print("FizzBuzz")
+        elif (num % 5 == 0):
+          print("Buzz")
+        elif (num % 3 == 0):
+          print("Fizz")
+        else:
+          print(num)    
+        num += 1
     pass
